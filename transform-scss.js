287fa76512ad4ba437d644c37c602af4c8e62b88
g_e_out <g_e@daum.net>
2024-06-26 11:03:10 +0900

    mod

--- transform-scss.js
+++ transform-scss.js
@@ -2,37 +2,66 @@
 const StyleDictionary = require("style-dictionary");
 
 // 이름 변환 함수
-function transformName(prop, removeFirstPathSegment) {
-  if (removeFirstPathSegment) {
+function transformName(prop, suffix) {
+  if (suffix) {
     const path = prop.path;
-    path[0] = removeFirstPathSegment
+    path[0] = suffix
     return path.join("-");
   } else {
-    return prop.path.slice(1).join("-");
+    return prop.path.slice(1).join("-"); // 첫 번째 요소 제외
+    // return prop.path.slice(-1).join("-"); // 마지막
+    // return prop.path.slice(-2).join("-"); // 끝에서 두 번째 
   }
 }
+
+// preset
+// const preset = `@charset "utf-8";\n\n//prefix\n$prefix: krds- !default;\n$box-shadow-base: 2px 2px 2px 0px rgba(0, 0, 0, 0.15) !default;\n\n`;
+const preset = `
+@charset "utf-8";
+
+
+//prefix
+$prefix: krds- !default;
+
+//transition
+$transition-base: .4s ease-in-out !default;
+$transition-fade: opacity .4s linear !default;
+$transition-collapse: max-height .4s ease !default;
+$transition-collapse-width: width .4s ease !default;
+
+//box shadow
+$box-shadow-base: 2px 2px 2px 0px rgba(0, 0, 0, 0.15) !default;
+
+
+`;
 
 // 커스텀 포매터 정의
 StyleDictionary.registerFormat({
   name: "custom/scss-format",
-  formatter: function(dictionary, config) {
+  formatter: function(dictionary) {
     const typeGroups = dictionary.allProperties.reduce((acc, prop) => {
       const type = prop.type;
+      // const type = prop.path[0];
+      // const type = prop.description;
       if (!acc[type]) {
         acc[type] = [];
       }
       const name = transformName(prop, "");
+      // const name = transformName(prop, type);
+      // acc[type].push(`  ${type}-${name}: ${prop.value},`);
       acc[type].push(`  ${name}: ${prop.value},`);
       return acc;
     }, {});
 
     const scssContent = Object.entries(typeGroups)
       .map(([type, tokens]) => {
-        return `$${type}s: (\n${tokens.join("\n")}\n);`;
+        // return `$${type}: (\n${tokens.join("\n")}\n);`;
+        return `//${type}\n$${type}: (\n${tokens.join("\n")}\n)!default;`;
       })
       .join("\n\n");
-
-    return scssContent;
+    
+    // return scssContent;
+    return preset + scssContent;
   }
 });
 
